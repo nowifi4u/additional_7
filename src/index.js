bdc26ae1af4c7447876b8746cceb6fc84c00689e
Shamber Maxim <36640388+nowifi4u@users.noreply.github.com>
2018-02-22 10:55:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (matrix[row][col] === 0) {      
+        let bi = ~~(row / 3) * 3;
+        let bj = ~~(col / 3) * 3;
+
+        let ready = []
+        for (let i = 0; i<9; i++){
+          ready.push(matrix[i][col])
+          ready.push(matrix[row][i])
+          ready.push(matrix[bi + i%3][bj + ~~(i/3)])
+        }
+        ready = ready.filter(num => num>0)
+        let candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(num => ready.indexOf(num) < 0);
+        
+        for (let i = 0; i < candidates.length; i++) {
+          matrix[row][col] = candidates[i];
+          let res = solveSudoku(matrix);
+          if (res !== false) return res;
+        }
+
+        matrix[row][col] = 0;
+        return false;
+      }
+    }
+  }
+  return matrix;
 }
